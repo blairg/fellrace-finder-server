ff1aba9328dbe8732203b0a0663ca0630d5bdbac
Blair Garrett <blair.garrett@sky.uk>
2019-05-05 15:42:57 +0100

    Fixing bug with not returning race categories properly

--- lib/services/raceService.ts
+++ lib/services/raceService.ts
@@ -559,7 +559,7 @@ export class RaceService implements RaceServiceInterface {
   private computeRaceType(climb: any, distance: any): string {
     const category = this.computeRaceCategory(climb.meters, distance.kilometers);
     let length;
-
+    
     if (!category) {
       return '';
     }
@@ -572,9 +572,23 @@ export class RaceService implements RaceServiceInterface {
       length = 'M';
     } 
 
-    if (distance.kilometers > 20) {
+    if (distance.kilometers >= 20) {
       length = 'L';
     } 
+
+    if (!category || !length) {
+      if (distance.miles > 0) {
+        const distanceParts = distance.miles.toString().split('.');
+
+        if (distanceParts.length > 1) {
+          return `${distanceParts[0]}.${distanceParts[1].substring(0, 1)} miles`;
+        }
+
+        return `${distanceParts[0]} miles`;
+      }
+
+      return '';
+    }
 
     return `${category}${length}`;
   }
